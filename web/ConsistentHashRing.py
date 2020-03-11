a280f6cfc3ad1d228b1792cd0df81e6adfbc277d
Utkarsh Sharma <utkarshsharma351@gmail.com>
2020-03-10 20:54:18 -0400

    First Commit

---
+++ web/ConsistentHashRing.py
@@ -0,0 +1,67 @@
+import md5
+import hashlib
+
+class ConsistentHashRing(object):
+    def __init__(self, nodes):
+        #Initialize it in this format: [['ip1:port1, key1'], ['ip2:port2, key2'], ..]
+        """Manages a hash ring.
+        `nodes` is a list of objects that have a proper __str__ representation.
+        `replicas` indicates how many virtual points should be used pr. node,
+        replicas are required to improve the distribution.
+        """
+        self.ring = dict()
+        self._sorted_keys = []
+        for node in nodes:
+            self.add_node(node["ip"], node["key"])
+
+    def add_node(self, node, key):
+        """Adds a `node` to the hash ring (including a number of replicas).
+        """
+        self.ring[key] = node
+        self._sorted_keys.append(key)
+        self._sorted_keys.sort()
+
+    def get_node(self, string_key):
+        """Given a string key a corresponding node in the hash ring is returned
+        along with it's position in the ring.
+        If the hash ring is empty, (`None`, `None`) is returned.
+        """
+
+        h_key = self.gen_key(string_key)
+        for _sorted_key in self._sorted_keys:
+            if h_key <= _sorted_key:
+                return self.ring[_sorted_key]
+        return self.ring[self._sorted_keys[0]]
+    
+    def gen_key(self, key):
+        """Given a string key it returns a long value,
+        this long value represents a place on the hash ring.
+        md5 is currently used because it mixes well.
+        """
+        return int(hashlib.md5(key).hexdigest(),16) % 10000
+
+'''
+    def remove_node(self, node):
+        """Removes `node` from the hash ring and its replicas.
+        """
+        for i in xrange(0, self.replicas):
+            key = self.gen_key('%s:%s' % (node, i))
+            del self.ring[key]
+            self._sorted_keys.remove(key)
+'''
+
+'''
+    def get_nodes(self, string_key):
+        """Given a string key it returns the nodes as a generator that can hold the key.
+        The generator is never ending and iterates through the ring
+        starting at the correct position.
+        """
+        if not self.ring:
+            yield None, None
+        node, pos = self.get_node_pos(string_key)
+        for key in self._sorted_keys[pos:]:
+            yield self.ring[key]
+        while True:
+            for key in self._sorted_keys:
+                yield self.ring[key]
+'''
---
+++ web/app.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+import requests
+from flask import Flask, session, request
+import logging
+import md5
+from werkzeug.contrib.cache import SimpleCache
+import json
+import ConsistentHashRing
+
+import sys
+
+cache = SimpleCache()
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+hash_ring = None
+
+with open('./config.json') as json_data_file:
+    data = json.load(json_data_file)
+    hash_ring = ConsistentHashRing.ConsistentHashRing(data["nodes"])
+
+@app.route('/route', methods=['GET'])
+def handle_route_get():
+    
+    node = hash_ring.get_node(request.args.get('key'))
+
+    url = "http://" + node + "/cache"
+    print('####NODE', hash_ring.ring, node, url, request.args.get('key'))
+    res = requests.get(url = url, params = {'key': request.args.get('key')}).text
+    print('hey', res)
+    return res
+
+
+@app.route('/route', methods=['POST'])
+def handle_route_post():
+    key = request.form.get('key')
+    val = request.form.get('val')
+    node = hash_ring.get_node(request.args.get('key'))
+    url = "http://" + node + "/cache" + ":5000"
+    return requests.post(url = url, data = {'key': request.args.get('key'), 'value': request.args.get('value')})
+
+#this method knows the key is here
+@app.route('/cache', methods=['GET'])
+def handle_cache_get():
+    #app.logger.info('Processing GET')
+    print('>?>>>entered here')
+    key = request.args.get('key')
+    val = cache.get(key)
+    res = val if val is not None else 'N/A'
+    print('bro', res)
+    return res
+
+#this method sets the key is here
+@app.route('/cache', methods=['POST'])
+def handle_cache_post():
+    #app.logger.info('Processing GET')
+    key = request.form.get('key')
+    val = request.form.get('value')
+    cache.set(key, val)
+    return 'Added key :' + key
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+# @app.route('/first_setup', methods=['POST'])
+# def handle_post():
+#     routing_info = requests.form['routing_info']
+#     curr_node = requests.form['curr_node']
+
+"""def create_hash(key):
+    #Given a string key, return a hash value.
+    return long(md5.md5(key).hexdigest(), 16)
+"""
