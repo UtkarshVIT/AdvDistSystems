b1ad0e7b316bd748b9a4da7ee8bc3e96c4523dcb
Utkarsh Sharma <utkarshsharma351@gmail.com>
2020-04-10 20:48:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,21 @@
-# install apache
-#sudo apt-get install apache2 \Q
-
 #expose public IP
 sudo iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport 80 -j ACCEPT
 sudo ufw allow 80
 sudo ufw allow 80
 sudo ufw reload
 
-#apt update
-sudo apt update
-
-#install apache
-sudo apt update \Q
+sudo apt update -y
 
 #install wsgi and python essentials
-sudo apt-get install libapache2-mod-wsgi python-dev \Q
-sudo a2enmod wsgi \Q
-
+sudo apt-get install libapache2-mod-wsgi python-dev -y
+sudo a2enmod wsgi -y
+# install apache
+sudo apt-get install apache2 -y
 #install python pip
-sudo apt-get install python-pip \Q
+sudo apt-get install python-pip -y
 
+cd /var/www/html/
+git clone https://github.com/UtkarshVIT/AdvDistSystems.git
 #Python modules
 sudo pip install flask
 sudo pip install requests
