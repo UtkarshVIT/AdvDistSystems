bcefe0fb7ef305462a05fe3344bfb86ae482ca28
Utkarsh Sharma <utkarshsharma351@gmail.com>
2020-04-11 17:05:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ sudo ufw allow 80
 sudo ufw reload
 sudo apt update -y
 
+sudo apt-get update -y
 # install apache
 sudo apt-get install apache2 -y
 #install wsgi and python essentials
@@ -25,5 +26,6 @@ sudo pip uninstall --yes Werkzeug
 sudo pip install Werkzeug==0.16.0
 sudo cp -f /var/www/html/AdvDistSystems/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 
+sudo a2enmod wsgi -y
 sudo service apache2 restart
 sudo tail -f /var/log/apache2/error.log
