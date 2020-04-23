2246116a3ff3d954fcca4f50aafd5efb52f1afb8
Utkarsh Sharma <utkarshsharma351@gmail.com>
2020-04-23 01:49:25 -0400

    MostStable

--- pytest.py
+++ pytest.py
@@ -21,38 +21,10 @@ key_map = {"bathtub": "bathtub1", "bed":"bed1", "bee":"bee1", "finger":"finger1"
 			"gas":"gas1", "gate":"gate1", "flower":"flower1", "mist":"mist1",\
 			"saddle":"saddle1", "ship":"ship1", "shoes":"shoes1", "roof":"roof1"}
 
-'''
-with open('final-json.txt', 'r') as f:
-	d = json.load(f)
-
-def send_get(key):
-	params = {"key": key}
-	r = requests.get(url = route_url, params = params)
-
-def send_post(key, val):
-	data = {"key": key, "val": val}
-	r = requests.post(url = route_url, data = data)
-
-def measure_perform_requests(k, percent_reads):
-	
-	keys_list = list(d.keys())[:k]
-	threads = []
-	start_time = time.time()
-	for key in keys_list[:int(k*(0.01*percent_reads))]:
-		threads.append(threading.Thread(target=send_get, args=(key,)))
-	for key in keys_list[:int(k*(0.01*(1.0-percent_reads)))]:
-		threads.append(threading.Thread(target=send_post, args=(key, d[key])))
-	for thread in threads:
-		thread.start()
-	for thread in threads:
-		thread.join()
-	print("k = ", k, "percent_reads = ", percent_reads, "total = ", str(time.time() - start_time), "avg = ", float(time.time() - start_time)/k)
-'''
-
-class TestStringMethods(unittest.TestCase):
+class TestScaling(unittest.TestCase):
 	#Testing insert into the system
-
-	def test_01_system_insert(self):
+	#@unittest.skip("skipiing this")
+	def test1(self):
 		for key in key_map:
 			data = {"key": key, "val": key_map[key]}
 			route_url = "http://" + lb_addr + "/route"
@@ -61,7 +33,8 @@ class TestStringMethods(unittest.TestCase):
 			self.assertEqual( r.text, "OK")
 
 	#Testing key based lookup from the system
-	def test_02_system_fetch(self):
+	#@unittest.skip("skipiing this")
+	def test2(self):
 		for key in key_map:
 			params = {"key": key}
 			route_url = "http://" + lb_addr + "/route"
@@ -71,16 +44,16 @@ class TestStringMethods(unittest.TestCase):
 
 	#Testing scale and broadcast
 	#@unittest.skip("skipiing this")
-	def test_03_system_scale_up(self):
+	def test3(self):
 		route_url = "http://" + lb_addr + "/add_node/5000/" + standby_addr
 		r = requests.get(url = route_url)
 		self.assertEqual(r.text, "OK")
-		
-		route_url = "http://" + standby_addr + "/fetch_keys"
-		r = requests.get(url = route_url)
-		result = ast.literal_eval(r.text)
-		result = sorted([obj[0] for obj in result])
-		self.assertEqual(result, ['4019', '4324', '4603'])
+		test_keys = ['bed', 'bathtub', 'bee']
+		for test_key in test_keys:
+			params = {"key": test_key}
+			route_url = "http://" + lb_addr + "/route_test"
+			result = requests.get(url = route_url, params = params).text
+			self.assertEqual(result, standby_addr)
 		for node in nodes:
 			url = "http://" + node + '/hash_ring'
 			result = json.loads(requests.get(url = url).text)
@@ -88,16 +61,17 @@ class TestStringMethods(unittest.TestCase):
 			self.assertEqual(keys, ['3000', '5000', '8000'])
 
 	#@unittest.skip("skipiing this")
-	def test_04_system_scale_down(self):
+	def test4(self):
 		route_url = "http://" + lb_addr + "/remove_node/" + removenode_addr
 		r = requests.get(url = route_url)
 		self.assertEqual(r.text, "OK")
-		
-		route_url = "http://" + migrated_to_addr + "/fetch_keys"
-		r = requests.get(url = route_url)
-		result = ast.literal_eval(r.text)
-		result = sorted([obj[0] for obj in result])
-		self.assertEqual(sorted(result), sorted(["8016","2184","9381","8155","6051","6823","8700","6952","9235"]))
+		test_keys = ['finger', 'gate', 'gas']
+		for test_key in test_keys:
+			params = {"key": test_key}
+			route_url = "http://" + lb_addr + "/route_test"
+			result = requests.get(url = route_url, params = params).text
+			self.assertEqual(result, migrated_to_addr)
+
 		for node in nodes:
 			url = "http://" + node + '/hash_ring'
 			result = json.loads(requests.get(url = url).text)
@@ -115,6 +89,22 @@ class TestStringMethods(unittest.TestCase):
 	# 	for i in range(1, 5):
 	# 		measure_perform_requests(i*100, 50)
 	# 	self.assertEqual(True, True)
+
+'''
+'roof' 2184     'gate' 6051    'mist' 8155
+'bed' 4019      'finger' 6823  'flower' 8700
+'bathtub' 4324  'gas' 6952     'shoes' 9235
+'bee' 4603      'saddle' 8016  'ship' 9381
+
+
+
+
+
+
+
+
+
+'''
 
 if __name__ == '__main__':
 	unittest.main()
